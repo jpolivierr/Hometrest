a8a10b56147eff5bceb76da5a086cb19f01db4a4
frederic <jpolivierr@gmail.com>
2023-03-24 14:36:20 -0400

    completed url parcer

--- client/src/Util/urlParcer.js
+++ client/src/Util/urlParcer.js
@@ -52,16 +52,29 @@ export const updateParam = (paramObj) =>{
     if(typeof paramObj === "object" && Object.keys(paramObj).length > 0){
         
         const url = new URL(window.location.href)
-         const search_params = url.searchParams
+
+        const search_params = url.searchParams
 
         console.log(search_params)
+
         for( const key in paramObj){
-         
+                
                 search_params.set(key,paramObj[key])
         }
 
         url.search = search_params.toString()
-         console.log(url.toString())
+
+        const data = search_params
+        const title = document.title
+        const newUrl = url.toString()
+
+        // This will create a new entry in the browser's history, without reloading
+        // window.history.pushState(data, title, newUrl);
+
+        // This will replace the current entry in the browser's history, without reloading
+        window.history.replaceState(data, title, newUrl);
+
+        console.log(title)
 
     }
 }
