b23ca9ada48a418d53d9fc5b058b926161e0988a
Frederic <olivierjp@protonmail.com>
2024-08-10 10:08:04 -0400

    Modified postbuild script

--- server/src/main/java/com/appvenir/hometrest/web/pages/Index.java
+++ server/src/main/java/com/appvenir/hometrest/web/pages/Index.java
@@ -1,14 +1,13 @@
 package com.appvenir.hometrest.web.pages;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Index {
 
     @GetMapping(value = "/{path:[^\\.]*}")
-    public String index(Model model){
+    public String home(){
         return "forward:/index.html";
     }
     
