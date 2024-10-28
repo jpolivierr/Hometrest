1416e3186d1690433a1fda94cb96c77e84fba6a4
Frederic <olivierjp@protonmail.com>
2024-10-28 10:30:06 -0400

    Add flyway.group to application.properties

--- src/main/java/com/appvenir/hometrest/App.java
+++ src/main/java/com/appvenir/hometrest/App.java
@@ -1,14 +1,32 @@
 package com.appvenir.hometrest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.appvenir.hometrest.domain.user.factory.UserFactory;
+import com.appvenir.hometrest.domain.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 // @SpringBootApplication
 @SpringBootApplication(scanBasePackages = {"com.appvenir"})
-public class App {
+@RequiredArgsConstructor
+public class App implements CommandLineRunner{
+
+	private final UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		var userRegistrationDto = UserFactory.getUserRegistrationDto();
+
+		userService.saveUser(userRegistrationDto);
 
 	}
 
